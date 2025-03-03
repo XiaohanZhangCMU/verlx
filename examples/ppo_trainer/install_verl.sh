82b0fa8634d0890e5d08e96c731d41bea84cce42
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2025-03-03 01:20:09 +0000

    update install script

--- examples/ppo_trainer/install_verl.sh
+++ examples/ppo_trainer/install_verl.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/volcengine/verl && cd verl && pip3 install -e .
+#git clone https://github.com/volcengine/verl && cd verl && pip3 install -e .
+# git clone https://github.com/XiaohanZhangCMU/verl.git
+cd verl && pip3 install -e .[databricks]
 cd ..
 git clone -b core_v0.4.0 https://github.com/NVIDIA/Megatron-LM.git
 cp verl/patches/megatron_v4.patch  Megatron-LM/
 cd Megatron-LM/
 git apply megatron_v4.patch
-vi megatron_v4.patch
 pip install -e .
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo $PYTHONPATH
 cd ..
-cd verl/
-vi examples/data_preprocess/gsm8k.py
-python
-python examples/data_preprocess/gsm8k.py --local_dir ~/data/gsm8k
-cd ..
-python download_model.py
+python verl/examples/data_preprocess/gsm8k.py --local_dir ~/data/gsm8k
+python verl/examples/ppo_trainer/download_model.py
 pip install opencv-fixer==0.2.5
 python -c "from opencv_fixer import AutoFix; AutoFix()"
 #sh verl/examples/ppo_trainer/run_qwen2.5-0.5b.sh
