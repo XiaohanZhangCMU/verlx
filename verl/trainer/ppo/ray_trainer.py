2551c65049588e402df00e5540881a17ac5fc0ac
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-03-24 11:47:11 -0700

    update

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -647,8 +647,11 @@ class RayPPOTrainer(object):
         self.wg_dicts = []
         for resource_pool, class_dict in self.resource_pool_to_cls.items():
             worker_dict_cls = create_colocated_worker_cls(class_dict=class_dict)
+            print("I am here 1004.1")
             wg_dict = self.ray_worker_group_cls(resource_pool=resource_pool, ray_cls_with_init=worker_dict_cls)
+            print("I am here 1004.2")
             spawn_wg = wg_dict.spawn(prefix_set=class_dict.keys())
+            print("I am here 1004.3")
             all_wg.update(spawn_wg)
             # keep the referece of WorkerDict to support ray >= 2.31. Ref: https://github.com/ray-project/ray/pull/45699
             self.wg_dicts.append(wg_dict)
