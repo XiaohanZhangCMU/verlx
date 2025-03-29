8cf1a14d07230041c4e6a8bda679f63435637392
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-03-28 23:35:03 -0700

    update

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -74,8 +74,6 @@ def initialize_ray_cluster():
 
     cdist.barrier()
 
-    print(f"I am here 0.0: {cdist.get_global_rank()=}, {cdist.get_local_rank()=}, {os.environ.get('CUDA_VISIBLE_DEVICES')=}, {os.environ.get('LOCAL_RANK', 0)=}")
-
     if cdist.get_local_rank() == 0 and cdist.get_global_rank() == 0:
         subprocess.run('ray start --head', shell=True)
         ray.init()
@@ -281,6 +279,8 @@ class TaskRunner:
 
 if __name__ == '__main__':
     initialize_ray_cluster()
+
+    print(f"I am here 0.0: {cdist.get_global_rank()=}, {cdist.get_local_rank()=}, {os.environ.get('CUDA_VISIBLE_DEVICES')=}, {os.environ.get('LOCAL_RANK', 0)=}")
 
     if cdist.get_global_rank() == 0:
         main()
