378aa20a9455028dd11f03fbb3fe0385e25e4b81
xiaohanzhangcmu <xiaohanzhang.cmu@gmail.com>
2025-03-30 20:57:09 -0700

    update

---
+++ verl/trainer/start_ray.py
@@ -0,0 +1,112 @@
+# Copyright 2024 Bytedance Ltd. and/or its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Note that we don't combine the main with ray_trainer as ray_trainer is used by other main.
+"""
+from verl.trainer.ppo.ray_trainer import RayPPOTrainer
+
+import os
+import ray
+import hydra
+
+import os
+import torch.distributed as dist
+from composer.utils import dist as cdist
+from composer.utils import get_device
+import torch
+import time
+import subprocess
+import ray
+import datetime
+import socket
+
+def get_ip():
+    """Retrieve the IP address of the current node."""
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
+    except Exception:
+        return "127.0.0.1"
+
+def get_local_rank():
+    """Retrieve the local rank of the process."""
+    return int(os.environ.get("LOCAL_RANK", 0))
+
+def get_global_rank():
+    """Retrieve the global rank of the process."""
+    return dist.get_rank() if dist.is_initialized() else 0
+
+def initialize_ray_cluster():
+
+    #dist.init_process_group(backend="nccl", timeout=datetime.timedelta(seconds=120))
+    #dist.init_process_group(backend="gloo", timeout=datetime.timedelta(seconds=120))
+    cdist.initialize_dist(get_device("cpu"), timeout=1800)
+
+    cdist.barrier()
+
+    #torch.cuda.set_device(f'cuda:{get_local_rank()}')
+
+    ip_address = get_ip()
+
+    # Gather IP addresses from all nodes
+    #gathered_ips = [None] * dist.get_world_size()
+    #dist.all_gather_object(gathered_ips, ip_address)
+    #head_ip_address = gathered_ips[0]  # Use rank 0 as head
+
+    head_ip_address = cdist.all_gather_object(ip_address)[0]
+
+    print(f"bigning debug {ip_address=}, {head_ip_address=}, {get_global_rank()=}")
+    #print(f"Rank {get_global_rank()} setting env vars: {os.environ['FLASH_ATTENTION_USE_TORCH']=},{os.environ['CUDA_LAUNCH_BLOCKING']=}, {os.environ['TORCH_USE_CUDA_DSA']=}, {os.environ['HYDRA_FULL_ERROR']=}, {os.environ['VLLM_ATTENTION_BACKEND']=}")
+
+    cdist.barrier()
+
+    if cdist.get_local_rank() == 0 and cdist.get_global_rank() == 0:
+        subprocess.run('ray start --head', shell=True)
+        ray.init()
+
+    cdist.barrier()
+
+    if cdist.get_local_rank() == 0 and cdist.get_global_rank() != 0:
+        time.sleep(10)
+        print(f"bigning debug {head_ip_address=}, {get_global_rank()=}")
+        subprocess.run(f'ray start --address {head_ip_address}:6379', shell=True)
+        print(f"bigning debug ray start done")
+        ray.init(address=f'{head_ip_address}:6379')
+
+    cdist.barrier()
+
+    if cdist.get_local_rank() == 0 and cdist.get_global_rank() == 0:
+        result = subprocess.run('ray status', shell=True, capture_output=True, text=True)
+        print(f"bigning debug {result=}")
+        print("ray cluster resources")
+        print(ray.cluster_resources())
+
+    cdist.barrier()
+    torch.distributed.destroy_process_group()
+
+    if cdist.get_local_rank() == 0 and cdist.get_global_rank() == 0:
+        result = subprocess.run('ray status', shell=True, capture_output=True, text=True)
+        print(f"bigning debug {result=}")
+        print("ray cluster resources")
+        print(ray.cluster_resources())
+
+
+if __name__ == '__main__':
+    initialize_ray_cluster()
+
+
+
